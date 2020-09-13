3931a46acdc6d927733a37427fd730f38387787c
t3chn0m4g3 <t3chn0m4g3@gmail.com>
2020-09-14 00:27:17 +0200

    tweaking

--- docker/elasticsearch/dist/gen_cert.sh
+++ docker/elasticsearch/dist/gen_cert.sh
@@ -2,7 +2,7 @@
 if [[ ! -f /data/elastic/certs/bundle.zip ]]; 
   then
     echo "First start, generating certs ..."
-    /usr/share/elasticsearch/bin/elasticsearch-certutil cert --silent --pem --in /usr/share/elasticsearch/config/instances.yml -out /data/elastic/certs/bundle.zip;
-    unzip /data/elastic/certs/bundle.zip -d /data/elastic/certs;
-    chown -R 1000:1000 /data/elastic/certs
+    /usr/share/elasticsearch/bin/elasticsearch-certutil cert --silent --pem --in /usr/share/elasticsearch/config/instances.yml -out /usr/share/elasticsearch/config/certs/bundle.zip;
+    unzip /usr/share/elasticsearch/config/certs/bundle.zip -d /usr/share/elasticsearch/config/certs;
+    #chown -R 1000:0 /data/elastic/certs
 fi;
